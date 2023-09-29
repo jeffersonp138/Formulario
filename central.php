004a9c18e918638030a7dc1b37fc2abd84eb0717
Jeff <jeffersonpereira138@gmail.com>
2023-09-29 17:39:51 -0300

    Update central.php
    Adicionei o dataTable em português.

--- central.php
+++ central.php
@@ -3,6 +3,8 @@ include('conexao.php');
 
 $sql = "SELECT * FROM lista ";
 $sql_query= $mysqli->query($sql) or die($mysqli->error);
+
+
 ?>
 
 <!DOCTYPE html>
@@ -11,24 +13,30 @@ $sql_query= $mysqli->query($sql) or die($mysqli->error);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Central</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <table border="1">
+    <link rel="stylesheet" href="estilo.css">
+    <link href="//cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
+    </head>
+<body id='tabela_body'>
+
+
+    <section>
+    <table id="tabela" border="1" >
+    
+    <thead >
         <tr>
-            <td>Código</td>
-            <td>nome</td>
-            <td>Sexo</td>
-            <td>E-mail</td>
-            <td>Telefone</td>
-            <td>Estado</td>
-            <td>Profissão</td>
-            <td>Mensagem</td>
-            <td>Opções</td>
+            <th>Nome</th>
+            <th>Sexo</th>
+            <th>E-mail</th>
+            <th>Telefone</th>
+            <th>Estado</th>
+            <th>Profissão</th>
+            <th>Mensagem</th>           
         </tr>
+    </thead>
+    
+    <tbody>
         <?php while($dado= $sql_query->fetch_array()){ ?>
         <tr>
-            <td><?=$dado['id']?></td>
             <td><?=$dado['nome']?></td>
             <td><?=$dado['sexo']?></td>
             <td><?=$dado['email']?></td>
@@ -36,14 +44,28 @@ $sql_query= $mysqli->query($sql) or die($mysqli->error);
             <td><?=$dado['estados']?></td>
             <td><?=$dado['profissao']?></td>
             <td><?=$dado['mensagem']?></td>
-            <td><a href="editar.php?codigo=<?php echo $dado['id']; ?>">Editar</a> 
-            <a href="excluir.php?codigo=<?php echo $dado['id']; ?>">Excluir</a> </td>
         </tr>
        
         <?php }?>
-    </table> 
-        
+    </tbody>
+    </table>  
+    </section>
+    <script
+    src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+    crossorigin="anonymous">
+    </script>
+    <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
-    </table>
+    <script>
+    $(document).ready(function(){
+        $('#tabela').DataTable({language: {
+        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json',
+    },
+        });
+    });
+    </script>
+
+    
 </body>
 </html>
